84f304df00863aa3fc5693e802cd16f93b61dda8
Dominik Mostek <mostekdominik@gmail.com>
2015-02-11 23:00:56 +0100

    Keeping fragment on rotation in drawer

--- app/src/main/java/dmostek/cz/library/HomeActivity.java
+++ app/src/main/java/dmostek/cz/library/HomeActivity.java
@@ -1,62 +1,35 @@
 package dmostek.cz.library;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.List;
 
 import dmostek.cz.library.libraryapi.SearchItemType;
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
-import it.neokree.materialnavigationdrawer.elements.listeners.MaterialSectionListener;
 
 public class HomeActivity extends MaterialNavigationDrawer implements BookThumbnailHolder.BookSelectedListener {
 
-    private static final String TAG_TASK_FRAGMENT = "home_search_fragment";
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home);
-//        FragmentManager fm = getSupportFragmentManager();
-//        if (savedInstanceState == null) {
-//            SearchTitlesFragment mTaskFragment = new SearchTitlesFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.home_search_fragment, mTaskFragment, TAG_TASK_FRAGMENT)
-//                    .commit();
-//        }
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void init(Bundle bundle) {
-        MaterialSection section = newSection("Test", new SearchTitlesFragment());
-        section.setTarget(new MaterialSectionListener() {
-            @Override
-            public void onClick(MaterialSection materialSection) {
-                MaterialSection currentSection = getCurrentSection();
-                boolean equals = materialSection.equals(currentSection);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        List<Fragment> fragments = supportFragmentManager.getFragments();
+        SearchTitlesFragment searchFragment = null;
+        if (fragments != null) {
+            for (Fragment fragment : fragments) {
+                if (fragment instanceof SearchTitlesFragment) {
+                    searchFragment = (SearchTitlesFragment) fragment;
+                }
             }
-        });
+        }
+        if (searchFragment == null) {
+            searchFragment = new SearchTitlesFragment();
+        }
+        MaterialSection section = newSection("Test", searchFragment);
         this.addSection(section);
         this.addBottomSection(newSection("Bottom Section", R.drawable.ic_settings_black_24dp, new Intent(this, Settings.class)));
     }
